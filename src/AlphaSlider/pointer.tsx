a9dc0eba0ac9182e83ade348c68faef6178350b3
dkarpovich <dkarpovich@navigine.com>
2020-05-05 20:55:11 +0300

    add alpha

---
+++ src/AlphaSlider/pointer.tsx
@@ -0,0 +1,20 @@
+/** @jsx jsx */
+import { css, jsx } from '@emotion/core';
+
+export const pointer = () => {
+  return (
+    <div
+    css={css`{
+          background: #fff;
+          width: 57px !important;
+          height: 22px !important;
+          border-radius: 12px !important;
+          margin-top: -10px;
+          margin-left: -28px;
+        }
+      `}
+    />       
+  )
+}
+
+export default pointer
